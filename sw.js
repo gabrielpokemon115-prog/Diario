4f97a48a3d26c561ac13af537dbcafa199b63bec
gabrielpokemon115-prog <gabrielpokemon11.5@gmail.com>
2026-03-03 01:12:16 -0300

    Up1

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'dark-diary-v1';
+const ASSETS = [
+  './diario.html',
+  './manifest.json',
+  './icon-480.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
